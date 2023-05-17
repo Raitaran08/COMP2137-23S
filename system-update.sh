3d56fffd17cb154e01e05e6fe623d779f66f4b1b
Raitaran08 <200515556@student.georgianc.on.ca>
2023-05-17 14:16:15 -0400

    created system update

---
+++ system-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# created may 17 by taran to perform a system update task for the lab
+
+#update the software cache in case it is needed 
+sudo apt update
+
+#upgrade using new software package version
+sudo apt upgrade
